ccbefaaceb0ab5082743295f530b7bede8003884
WorldIT-academy <worldit_academy@worldit.academy>
2025-01-18 21:09:12 +0200

    Work with context

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,4 @@ from django.urls import path
 
 urlpatterns = [
     path('', render_home, name= 'home'),
-
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Users
 
 # Create your views here.
 def render_home(request):
-    return render(request= request, template_name= 'home/home.html')
+
+    all_users = Users.objects.all()
+    
+    return render(
+        request= request, 
+        template_name= 'home/home.html',
+        context= {
+            'title': 'Home Page',  # This will be rendered in the template as {{ title }}
+            'user_name': all_users[0].name
+        } 
+    )
