a771592e63f363a3a5415b4d8236232e5a7ebe18
WorldIT-academy <worldit_academy@worldit.academy>
2025-01-25 21:31:07 +0200

    we created models Post and Author. Add connection Post-Author, type One-to-Many

--- post/models.py
+++ post/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length= 255)
+    last_name = models.CharField(max_length= 255)
+    age = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
+    
+class Post(models.Model):
+    title = models.CharField(max_length= 255)
+    content =  models.TextField()
+    is_published = models.BooleanField(default= True)
+    
+    author = models.ForeignKey(to = Author, on_delete= models.CASCADE, null= True)
+    
+    
--- post/views.py
+++ post/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post, Author
 # Create your views here.
 def render_post(request):
-    return render(request= request, template_name= 'post/post.html')
+    all_post = Post.objects.all()
+    return render(
+        request= request, 
+        template_name= 'post/post.html',
+        context= {
+            "posts": all_post
+        }
+    )
