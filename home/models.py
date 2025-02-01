80d3ab70c38612bc9f55fbf8b30c24ad957fa117
WorldIT-academy <worldit_academy@worldit.academy>
2025-02-01 21:07:29 +0200

    create superuser

--- home/models.py
+++ home/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+#
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Users(models.Model):
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    
     name = models.CharField(max_length= 50)
     last_name = models.CharField(max_length= 50)
     age = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return f"{self.name} {self.last_name}"
--- post/models.py
+++ post/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-
+from home.models import Users
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length= 255)
-    last_name = models.CharField(max_length= 255)
-    age = models.IntegerField()
+    # name = models.CharField(max_length= 255)
+    # last_name = models.CharField(max_length= 255)
+    # age = models.IntegerField()
+    
+    user = models.ForeignKey(to= Users, on_delete= models.CASCADE, null= True)
     
     def __str__(self):
-        return f"{self.name} {self.last_name}"
+        return f"{self.user.name} {self.user.last_name}"
+    
+    
     
 class Post(models.Model):
     title = models.CharField(max_length= 255)
