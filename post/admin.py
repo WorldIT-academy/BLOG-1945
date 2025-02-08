2d22c67c91719ff6e7e0bbd8aa78c58976f171f9
WorldIT-academy <worldit_academy@worldit.academy>
2025-02-08 21:16:05 +0200

    create field ManytoMany

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Author, Post
+from .models import Author, Post, Tag
 
 # Register your models here.
 admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Tag)
+
--- post/models.py
+++ post/models.py
@@ -2,22 +2,28 @@ from django.db import models
 from home.models import Users
 # Create your models here.
 class Author(models.Model):
-    # name = models.CharField(max_length= 255)
-    # last_name = models.CharField(max_length= 255)
-    # age = models.IntegerField()
     
     user = models.ForeignKey(to= Users, on_delete= models.CASCADE, null= True)
     
     def __str__(self):
         return f"{self.user.name} {self.user.last_name}"
     
-    
-    
 class Post(models.Model):
     title = models.CharField(max_length= 255)
     content =  models.TextField()
     is_published = models.BooleanField(default= True)
-    
+    #
     author = models.ForeignKey(to = Author, on_delete= models.CASCADE, null= True)
+    #
+    tags = models.ManyToManyField('Tag')
+    
+    def __str__(self):
+        return f'{self.title}'
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
     
     
